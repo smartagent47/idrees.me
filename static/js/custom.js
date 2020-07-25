1337f98e697c76975fe4363ecf7a84a5d9e3a1f6
Idrees <dargahidrees@gmail.com>
2020-07-25 21:55:37 +0530

    Some jquery

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,22 @@
-// Making navigation sticky on scroll
-$('#nav').sticky({ topSpacing: 0 });
-$('#nav ul').onePageNav({
-  scrollSpeed: 400,
-  currentClass: 'current',
-  easing: 'swing',
-});
+if (window.location.pathname.indexOf('pages') > -1) {
+  // Random color change of header in our project pages
+  let colorArray = ['#ffea00', '#000'];
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() < $(window).height() / 2) {
-    $('#nav').find('li').removeClass('current');
-  }
-});
+  let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+  $('.project-header').css('background', randomColor);
+  $('meta[name=theme-color]').attr('content', randomColor);
+} else {
+  // Making navigation sticky on scroll
+  $('#nav').sticky({ topSpacing: 0 });
+  $('#nav ul').onePageNav({
+    scrollSpeed: 400,
+    currentClass: 'current',
+    easing: 'swing',
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() < $(window).height() / 2) {
+      $('#nav').find('li').removeClass('current');
+    }
+  });
+}
